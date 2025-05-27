9d3ef548283eb13a1b1dbf32b4c08c4f52230a01
shivamsharma-23 <shivamsharma050523@gmail.com>
2025-05-27 11:10:55 +0530

    update on 05/27/2025 11:10:55

--- 7POINTERS/pointer.cpp
+++ 7POINTERS/pointer.cpp
@@ -20,21 +20,42 @@ int main(){
     // cout<<"at *a"<<*a<<endl; //error
 
 
-    int a =5;
-    char ch = 'b';
-    double db =5.005;
+    // int a =5;
+    // char ch = 'b';
+    // double db =5.005;
 
-    int* p = &a;
-    char* c = &ch;
-    double* d = &db;
+    // int* p = &a;
+    // char* c = &ch;
+    // double* d = &db;
 
-    cout<<sizeof(db)<<endl;
-    cout<<sizeof(a)<<endl;
-    cout<<sizeof(ch)<<endl;
-    cout<<sizeof(p)<<endl;
-    cout<<sizeof(c)<<endl;
-    cout<<sizeof(d)<<endl;
+    // cout<<sizeof(db)<<endl;
+    // cout<<sizeof(a)<<endl;
+    // cout<<sizeof(ch)<<endl;
+    // cout<<sizeof(p)<<endl;
+    // cout<<sizeof(c)<<endl;
+    // cout<<sizeof(d)<<endl;
+
+    int arr[4] = {10,20,30,40};
+
+    int* p =arr;
+
+    // int* ptr = arr;
+    // cout<<" arr: "<<arr<<endl;
+    // cout<<" *arr: "<<*arr<<endl;
+    // cout<<" arr[0] "<<arr[0]<<endl;
+    // cout<<" &arr[0]: "<<&arr[0]<<endl;
+    // cout<<" &arr: "<<&arr<<endl;
+    // cout<<" *arr[0]: "<<*arr[0]<<endl; -- error
 
 
+    int i =3;
 
-} 
+    cout<<"i[arr]: "<<i[arr]<<endl;
+
+    cout<<"arr[i]: "<<arr[i]<<endl;
+
+    cout<<"*(p) + 1: " <<*(p) + 1<<endl; //index m naahi value m ek add hora hai
+
+    cout<<"*(p+ 1): " <<*(p+ 1)<<endl;
+
+}
