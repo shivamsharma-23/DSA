ba2195e7ee6db1ede78ad4d3bb6c22f791926324
shivamsharma-23 <shivamsharma050523@gmail.com>
2025-06-02 12:34:57 +0530

    update on 06/02/2025 12:34:57

--- 8RECURSION/rec2.cpp
+++ 8RECURSION/rec2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 
 
@@ -30,24 +31,103 @@ using namespace std;
 
 //PRINTING ARRAY USING RECURRSION 
 
-void print(int arr[],int n,int i){
+// void print(int arr[],int n,int i){
 
-//BASE CONDITION
-if(i>=n)
-return;
+// //BASE CONDITION
+// if(i>=n)
+// return;
 
-//1 case solve krdee
-cout<<arr[i]<<" ";
+// //1 case solve krdee
+// cout<<arr[i]<<" ";
 
-//baaki recursion pe  chod dee
-print(arr,n,i+1);
+// //baaki recursion pe  chod dee
+// print(arr,n,i+1);
+
+// }
+// int main(){
+//     int arr[5] = {10,20,30,40,50};
+//     int n = 5;
+
+//     int i = 0;
+
+//     print(arr,n,i);
+    
+// }
+
+// =============================================================================================
+
+
+// MAXIMUM ELEMNT USING RECURSION 
+
+// void findMaxi(int arr[] , int n ,int i, int &maxi){
+
+//     //1 base condition 
+//     if(i>=n){
+
+//      //agar iteration array k size se jyada higyii tohh stop kar jaooo
+
+//         return;
+    
+//     }
+
+//     // 1 case sambahal loo ek case solve krdoo
+//     if(arr[i] > maxi){
+//         maxi = arr[i];
+//     }
+
+
+//     //baaki recursion sambahal lega
+//     findMaxi(arr,n,i+1,maxi);
+
+// }
+// int main(){
+//     int arr[] = {1,3,44,5,6,664,23,44,5,62,4543,4};
+//     int n = 12;
+//     int i = 0;
+//     int maxi = INT_MIN;
+
+
+//     findMaxi(arr,n,i,maxi);
+
+//     cout<<"maximum number in the array is: "<<maxi<<endl;
+// }
+
+// this is a comparison operator (==) instead of the assignment operator (=).
+// You meant to assign arr[i] to maxi when arr[i] > maxi.
+
+
+// ============================================
+//MINIMUM NUMBER 
+
+void findMini(int arr[] , int n ,int i, int &mini){
+
+    //1 base condition 
+    if(i>=n){
+
+     //agar iteration array k size se jyada higyii tohh stop kar jaooo
+
+        return;
+    
+    }
+
+    // 1 case sambahal loo ek case solve krdoo
+    if(arr[i] < mini){
+        mini = arr[i];
+    }
+
+
+    //baaki recursion sambahal lega
+    findMini(arr,n,i+1,mini);
 
 }
 int main(){
-    int arr[5] = {10,20,30,40,50};
-    int n = 5;
-
+    int arr[] = {1,3,44,5,6,664,23,44,5,62,4543,4};
+    int n = 12;
     int i = 0;
+    int mini = INT_MAX;
 
-    print(arr,n,i);
+
+    findMini(arr,n,i,mini);
+
+    cout<<"minimum number in the array is: "<<mini<<endl;
 }
