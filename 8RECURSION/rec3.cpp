3891a3149b1897c660c97fe16dd54284d49635ed
shivamsharma-23 <shivamsharma050523@gmail.com>
2025-06-03 12:22:33 +0530

    update on 06/03/2025 12:22:33

--- 8RECURSION/rec3.cpp
+++ 8RECURSION/rec3.cpp
@@ -189,3 +189,58 @@ using namespace std;
 
 
 
+// ======================================== BINARY SEARCH USING RECURSSION =============================
+
+// ------- ASSIGNMENT(=JAB KISI VALUE KO KISI DUSRI VALUE M PASS KRVANA HOO) AND COMARISON OPERSTOR(== JAB KINI DO VALUE KI QUANTITY CHECK KRNI HI SAME HAI K NAHI ) KA DHYAAN RAKHH 
+
+// int binarySearch(vector<int>& v,int s ,int e ,int& key){
+
+//     int mid = s + (e-s)/2;
+
+//     //BASE CASEEE 1 kab rukna hai ---- AGAR KEY IS NOT PRESENT OR 1 ELEMNT ONLY
+//     if(s>e){
+//         return -1;
+//     }
+
+//     // CASE 2 -- YAA FIR MID MIL JAYE
+//     if(v[mid] == key){
+//         return mid;
+//     }
+
+
+//     // 1 case solve krna hai baaki recurrsion sambhal lega
+    
+//     // v[mid] < key --------- right m search karengee
+//     if(v[mid] < key){
+//         return binarySearch(v,mid+1,e,key);
+//     }
+
+//     //v[mid] > key ------------ left search
+//     else{
+
+//         return binarySearch(v,s,mid-1,key);
+//     }
+// }
+
+
+// int main(){
+//     vector <int>v{10,20,30,40,50,60};
+
+//     int n =  v.size();
+//     int s = 0;
+
+//     int e =n-1;
+
+//     int key = 60;
+
+//     int ans = binarySearch(v,s,e,key);
+
+//     if(ans != -1){
+//         cout<<"key found at: "<<ans;
+//     }
+
+//     else{
+
+//         cout<<"key not found";
+//     }
+// }
